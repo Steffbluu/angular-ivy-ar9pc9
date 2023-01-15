847f5622d16a78143f70aaf79df9ed848bdcbbc0
Steffbluu <86385328+Steffbluu@users.noreply.github.com>
2023-01-15 20:28:13 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { TodosComponent } from './components/todos/todos.component';
+import { TodosComponent } from './components/todos.component';
 
 @NgModule({
   declarations: [AppComponent, TodosComponent],
